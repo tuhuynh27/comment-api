cb950913fa689a8685bc5f10acb5353b31b61fb4
Tu Huynh <tuhuynh@Tus-MacBook-Pro.local>
2018-08-17 17:20:20 +0700

    Update

--- app/middlewares/CORSMiddleware.go
+++ app/middlewares/CORSMiddleware.go
@@ -2,21 +2,10 @@ package middlewares
 
 import "github.com/rs/cors"
 
-// // CORSMiddleware ...
-// type CORSMiddleware struct{}
-
-// func (c *CORSMiddleware) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-// 	next(rw, r)
-
-// 	rw.Header().Set("Access-Control-Allow-Origin", "*")
-// 	rw.Header().Set("Access-Control-Allow-Origin", "POST, GET, OPTIONS, PUT, DELETE")
-// 	rw.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
-// 	rw.Header().Set("Access-Control-Allow-Credentials", "true")
-// }
-
 // CORSMiddleware ...
 func CORSMiddleware() *cors.Cors {
 	return cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
 		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE"},
 	})
 }
